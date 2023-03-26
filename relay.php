690959f3676b4e9a59249261ce874ef6595885ea
Tamas Kalman <ktamas77@gmail.com>
2023-03-26 00:55:20 -0700

    Comments added

--- relay.php
+++ relay.php
@@ -1,5 +1,7 @@
 <?php
 
+// Sets Relay Channel on/off on the given channel, with optional timer (built-in to the relay)
+// Return value is an 8 characters string, contains each relays' status as 0 or 1
 function setRelay($relayNumber, $on, $delaySeconds = 0, $relayHost = '192.168.1.100', $relayPort = 6722) {
     $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
     $result = socket_connect($socket, $relayHost, $relayPort);
@@ -18,6 +20,4 @@ function setRelay($relayNumber, $on, $delaySeconds = 0, $relayHost = '192.168.1.
 }
 
 // turns on relay 1 fro 30 seconds, then off
-setRelay(1, true, 30);
-
-
+$status = setRelay(1, true, 30);
